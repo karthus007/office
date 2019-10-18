1cb8bce626cc898e61f5d903730b2a4e33f88ef0
xujinqiang <xujinqiang@hgandin.com>
2019-10-18 19:03:15 +0800

    add

--- src/main/java/com/andin/controller/OfficeController.java
+++ src/main/java/com/andin/controller/OfficeController.java
@@ -1,16 +1,21 @@
 package com.andin.controller;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.andin.service.OfficeService;
@@ -27,18 +32,27 @@ public class OfficeController {
 	
 	@RequestMapping("/upload")
 	@ResponseBody
-	public Map<String, Object> upload(HttpServletRequest req){
+	public Map<String, Object> upload(HttpServletRequest req, @RequestParam("file") Part part){
 		logger.debug("TestController.getAppInfo method execute is start...");
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
 			String path = req.getServletContext().getRealPath("/") + "upload/";
+			InputStream in = part.getInputStream();
+			OutputStream os = new FileOutputStream(path + part.getSubmittedFileName());
+			byte[] b = new byte[1024*4];
+			int len = 0;
+			while ((len = in.read(b)) != -1) {
+				os.write(b, 0, len);
+			}
+			in.close();
+			os.close();
 			map.put(ConstantUtil.RESULT_CODE, ConstantUtil.DEFAULT_SUCCESS_CODE);
 			map.put(ConstantUtil.RESULT_MSG, ConstantUtil.DEFAULT_SUCCESS_MSG);
 			logger.debug("TestController.getAppInfo method execute is successful...");
 		} catch (Exception e) {
 			map.put(ConstantUtil.RESULT_CODE, ConstantUtil.DEFAULT_ERROR_CODE);
 			map.put(ConstantUtil.RESULT_MSG, ConstantUtil.DEFAULT_ERROR_MSG);
-			logger.error("TestController.getAppInfo method execute is error: ", e.getMessage());
+			logger.error("TestController.getAppInfo method execute is error: ", e);
 		}
 		return map;
 	}
@@ -51,6 +65,7 @@ public class OfficeController {
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
 			String name = URLDecoder.decode(req.getParameter("name"), "UTF-8");
+			System.out.println(name);
 			map.put(ConstantUtil.RESULT_CODE, ConstantUtil.DEFAULT_SUCCESS_CODE);
 			map.put(ConstantUtil.RESULT_MSG, ConstantUtil.DEFAULT_SUCCESS_MSG);
 			logger.debug("TestController.getAppInfo method execute is successful...");
