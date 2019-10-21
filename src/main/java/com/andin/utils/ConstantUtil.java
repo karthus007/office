2db1f4f5cb78f8a0ad9711b8728016ef86f14d5d
xujinqiang <xujinqiang@hgandin.com>
2019-10-21 09:41:24 +0800

    add

--- src/main/java/com/andin/utils/ConstantUtil.java
+++ src/main/java/com/andin/utils/ConstantUtil.java
@@ -42,4 +42,38 @@ public class ConstantUtil {
 	public static final String CONFIG_PROPERTIES = "config";
 	
 	
+	/********************* 上传文件路径常量 *********************/
+	
+	public static final String DOCX = "docx";
+	
+	public static final String DOC = "doc";
+	
+	public static final String XLSX = "xlsx";
+	
+	public static final String XLS = "xls";
+	
+	public static final String PPTX = "pptx";
+	
+	public static final String PPT = "ppt";
+	
+	public static final String DOCX_PATH = "docx/";
+	
+	public static final String XLSX_PATH = "xlsx/";
+	
+	public static final String PPTX_PATH = "pptx/";
+	
+	public static final String HTML_DOCX_PATH = "html/docx/";
+	
+	public static final String HTML_XLSX_PATH = "html/xlsx/";
+	
+	public static final String HTML_PPTX_PATH = "html/pptx/";
+	
+	public static final String PDF_DOCX_PATH = "pdf/docx/";
+	
+	public static final String PDF_XLSX_PATH = "pdf/xlsx/";
+	
+	public static final String PDF_PPTX_PATH = "pdf/pptx/";
+	
+	
+	
 }
