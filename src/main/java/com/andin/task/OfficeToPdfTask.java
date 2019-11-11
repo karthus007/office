96cb38f4b4599aca2642b834dfbb1919a4ca6fb3
xujinqiang <xujinqiang@hgandin.com>
2019-11-11 09:08:40 +0800

    add

--- src/main/java/com/andin/task/OfficeToPdfTask.java
+++ src/main/java/com/andin/task/OfficeToPdfTask.java
@@ -1,5 +1,8 @@
 package com.andin.task;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -20,6 +23,8 @@ import com.andin.utils.StringUtil;
 @EnableScheduling
 public class OfficeToPdfTask {
 	
+    private static ExecutorService pool = Executors.newFixedThreadPool(10);
+	
     private static Logger logger = LoggerFactory.getLogger(OfficeToPdfTask.class);
 
 	@Scheduled(cron = "*/5 * * * * ?")/** 每五秒触发一次 **/
@@ -28,6 +33,26 @@ public class OfficeToPdfTask {
 		//获取任务列表
 		TaskModel task = HttpClientUtil.getTask();
 		if(task != null) {
+			TaskThread thread = new TaskThread(task);
+			pool.execute(thread);
+		}
+	}
+	
+	/**
+	  * 启动线程转换任务
+	 * @author Administrator
+	 *
+	 */
+	public class TaskThread extends Thread{
+		
+		private TaskModel task;
+		
+		public TaskThread(TaskModel task) {
+			this.task = task;
+		}
+		
+		@Override 
+		public void run() {
 			Boolean downloadResult = false;
 			Boolean officeToPdfResult = false;
 			Boolean uploadResult = false;
@@ -56,7 +81,7 @@ public class OfficeToPdfTask {
 			String result = "[downloadResult=" + downloadResult + "], [officeToPdfResult=" + officeToPdfResult + "], [uploadResult=" + uploadResult + "], [updateResult=" + updateResult + "]";
 			logger.debug("OfficeToPdfTask.getOfficeTaskListToPdf method executed task result is: " + result);
 		}
-		logger.debug("OfficeToPdfTask.getOfficeTaskListToPdf method executed is end...");
+		
 	}
 	
 }
