c8ad15de01485798adda93d80257230b19c4a636
xujinqiang <xujinqiang@hgandin.com>
2019-11-20 10:47:59 +0800

    add

--- src/main/java/com/andin/filter/OfficeFilter.java
+++ src/main/java/com/andin/filter/OfficeFilter.java
@@ -27,7 +27,7 @@ public class OfficeFilter implements Filter{
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		String uri = req.getRequestURI();
-		if(uri.startsWith("/license") || uri.contains(".")) {
+		if(uri.contains("/license") || uri.contains(".")) {
 			chain.doFilter(request, response);			
 		}else {
 			if(CommonUtil.LICENSE_STATUS) {
